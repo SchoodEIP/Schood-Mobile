b32311af3f4d6368a7f2e70d7de9b43eae0f4462
Mateo Deroche <mateo.deroche@epitech.eu>
2023-12-03 20:01:20 -0800

    [+] add api call

--- lib/Survey/SurveyScreen.dart
+++ lib/Survey/SurveyScreen.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 import 'dart:ffi';
 
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:schood/utils/BottomBarApp.dart';
 import 'package:schood/style/AppColors.dart';
@@ -28,17 +29,39 @@ class _SurveyState extends State<SurveyScreen> {
   bool? doIHaveProblemsWithClassmates;
   String id = global.globalToken;
 
-  @override
-  _getSurvey(BuildContext context) async {
+ Future<List<Map<String, dynamic>>?> _getSurveyData(BuildContext context) async {
     final getdata = GetClass();
-    print(global.globalToken);
-    final response = await getdata.getData(global.globalToken, "student/questionnaire");
+    final response = await getdata.getData(global.globalToken, "shared/questionnaire");
+
+    if (response.statusCode == 200) {
+      try {
+        final surveyData = jsonDecode(response.body);
+        print(surveyData);
+      } catch (e) {
+        print('Erreur lors du décodage du JSON : $e');
+      }
+    } else {
+      print(
+          'Erreur lors de la récupération des données : ${response.statusCode}');
+    }
+    return null;
   }
 
+  @override
+  void initState() {
+    _getSurveyData(context);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    _getSurvey(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(
+        title: const H1TextApp(
+          text: "Questionnaire",
+          color: AppColors.backgroundDarkmode,
+        ),
         backgroundColor: Colors.transparent,
         automaticallyImplyLeading: false,
         elevation: 0.0,
@@ -54,204 +77,37 @@ class _SurveyState extends State<SurveyScreen> {
             ),
           ),
         ],
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: AppColors.purpleSchood),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
+      backgroundColor: themeProvider.getBackgroundColor(),
       body: SingleChildScrollView(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //       const H3TextApp(
-      //         text: "Comment te sens-tu aujourd'hui ?",
-      //       ),
-      //       const SizedBox(
-      //         height: 30,
-      //       ),
-      //       Row(
-      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //         children: [
-      //           Column(
-      //             children: [
-      //               IconButton(onPressed: () {}, icon: const Icon(Icons.mood)),
-      //               MoodOption(
-      //                 mood: 'Good',
-      //                 isSelected: selectedMoods.contains('Good'),
-      //                 onSelected: (isSelected) {
-      //                   setState(() {
-      //                     if (isSelected) {
-      //                       selectedMoods.add('Good');
-      //                     } else {
-      //                       selectedMoods.remove('Good');
-      //                     }
-      //                   });
-      //                 },
-      //               ),
-      //             ],
-      //           ),
-      //           Column(
-      //             children: [
-      //               IconButton(onPressed: () {}, icon: const Icon(Icons.face)),
-      //               MoodOption(
-      //                 mood: 'Neutral',
-      //                 isSelected: selectedMoods.contains('Neutral'),
-      //                 onSelected: (isSelected) {
-      //                   setState(() {
-      //                     if (isSelected) {
-      //                       selectedMoods.add('Neutral');
-      //                     } else {
-      //                       selectedMoods.remove('Neutral');
-      //                     }
-      //                   });
-      //                 },
-      //               ),
-      //             ],
-      //           ),
-      //           Column(
-      //             children: [
-      //               IconButton(
-      //                   onPressed: () {}, icon: const Icon(Icons.mood_bad)),
-      //               MoodOption(
-      //                 mood: 'Bad',
-      //                 isSelected: selectedMoods.contains('Bad'),
-      //                 onSelected: (isSelected) {
-      //                   setState(() {
-      //                     if (isSelected) {
-      //                       selectedMoods.add('Bad');
-      //                     } else {
-      //                       selectedMoods.remove('Bad');
-      //                     }
-      //                   });
-      //                 },
-      //               ),
-      //             ],
-      //           ),
-      //         ],
-      //       ),
-      //       const SizedBox(height: 30),
-      //       const H3TextApp(
-      //         text: "Suis je en bonne forme ?",
-      //       ),
-      //       Row(
-      //         children: [
-      //           Radio<bool>(
-      //             value: true,
-      //             groupValue: amIGoodShape,
-      //             onChanged: (value) {
-      //               setState(() {
-      //                 amIGoodShape = value;
-      //               });
-      //             },
-      //           ),
-      //           const H3TextApp(text: "Oui"),
-      //           Radio<bool>(
-      //             value: false,
-      //             groupValue: amIGoodShape,
-      //             onChanged: (value) {
-      //               setState(() {
-      //                 amIGoodShape = value;
-      //               });
-      //             },
-      //           ),
-      //           const H3TextApp(text: "Non"),
-      //         ],
-      //       ),
-      //       const SizedBox(height: 30),
-      //       const H3TextApp(
-      //         text: "Suis je satisfait de ma semaine ?",
-      //       ),
-      //       Row(
-      //         children: [
-      //           Radio<bool>(
-      //             value: true,
-      //             groupValue: amISatisfied,
-      //             onChanged: (value) {
-      //               setState(() {
-      //                 amISatisfied = value;
-      //               });
-      //             },
-      //           ),
-      //           const H3TextApp(text: "Oui"),
-      //           Radio<bool>(
-      //             value: false,
-      //             groupValue: amISatisfied,
-      //             onChanged: (value) {
-      //               setState(() {
-      //                 amISatisfied = value;
-      //               });
-      //             },
-      //           ),
-      //           const H3TextApp(
-      //             text: "Non",
-      //           ),
-      //         ],
-      //       ),
-      //       const SizedBox(height: 30),
-      //       const H3TextApp(
-      //         text: "Les cours se déroulent-ils bien ?",
-      //       ),
-      //       const SizedBox(height: 30),
-      //       const H3TextApp(
-      //         text: "Ai je l'impression d'avoir des difficultés ?",
-      //       ),
-      //       const SizedBox(height: 30),
-      //       const H3TextApp(
-      //         text: "Ai je des problèmes avec mes camarades de classe ?",
-      //       ),
-      //       const SizedBox(height: 30),
-      //       ElevatedButton(
-      //         onPressed: () {
-      //           // Envoyer le formulaire avec les humeurs sélectionnées
-      //           if (selectedMoods.isNotEmpty) {
-      //             // Ici, vous pouvez envoyer les données du formulaire à une API ou effectuer d'autres actions nécessaires.
-      //           } else {}
-      //         },
-      //         child: const Text('Submit'),
-      //       ),
-      //     ],
-      //   ),
+          child: FutureBuilder<List<Map<String, dynamic>>?> (
+            future: _getSurveyData(context),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  // DISPLAY QUESTIONS
+                  return ListView(
+                    shrinkWrap: true,
+                    children: const [
+                      H3TextApp(
+                        text: "Comment te sens-tu aujourd'hui ?",
+                      ),
+                    ]
+                  );
+                }
+          ),
       ),
       bottomNavigationBar: const BottomBarApp(
         indexapp: 1,
         //Userinfo: widget.Userinfo,
       ),
-    );
-  }
-}
-
-class MoodOption extends StatefulWidget {
-  final String mood;
-  final bool isSelected;
-  final ValueChanged<bool> onSelected;
-
-  const MoodOption({
-    super.key,
-    required this.mood,
-    required this.isSelected,
-    required this.onSelected,
-  });
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _MoodOptionState createState() => _MoodOptionState();
-}
-
-class _MoodOptionState extends State<MoodOption> {
-  bool _isSelected = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _isSelected = widget.isSelected;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: _isSelected,
-      onChanged: (newValue) {
-        setState(() {
-          _isSelected = newValue!;
-          widget.onSelected(newValue);
-        });
-      },
     );
   }
 }
